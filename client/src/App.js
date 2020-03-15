ed372329266c91ad3755227b5ac4edc364afaaa1
AxelElg <axelelgenstiern@gmail.com>
2020-03-15 22:18:04 +0100

    memory game working somwhat, next steap is to refine the game further

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React , { useState } from 'react';
 import './App.css';
+import GameBoard from './GameBoard.js'
 
-function App() {
+
+export default function App() {
+
+  const [gameOn, setGameOn] = useState(false)
   return (
     <div className="App">
+    { gameOn ?
+      < GameBoard />:
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>Welcome to MemoryGif</h1>
+        <h3>the rules are simple and as follows</h3>
+        <ul>
+          <li>Click on a card to flip it</li>
+          <li>Try to find the same card twise to pair them up</li>
+          <li>Profit!!!!!</li>
+        </ul>
+        <button onClick={ () => setGameOn(true)}>
+          click me to start the game
+        </button> 
+      </header> }
     </div>
   );
 }
-
-export default App;
--- index.js
+++ index.js
@@ -1,34 +1,55 @@
 const express = require('express');
 const path = require('path');
-const http = require('http');
-
+const fs = require('fs');
+const fetch = require("node-fetch");
 const app = express();
+const dbDir = './db/gifs.json';
 
-const Http = new XMLHttpRequest();
-const url=' .';
-Http.onreadystatechange = () => {
-    if (xmlHttp.readyState == 4 && xmlHttp.status == 200) callback(xmlHttp.responseText);
-    console.log(Http.responseText);
+/* Randomize array in-place using Durstenfeld shuffle algorithm */
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
 }
-Http.open("GET", url, true);
-Http.send(null);
 
+fetch('')
+    .then(res => {
+        if (res.status >= 400) {
+        throw new Error("Bad response from server");
+        }
+        return res.json();
+    })
+    .then(res => res.data)
+    .then(data => {
+        const dataArr = [];
+        data.forEach(element => {
+            dataArr.push(element.images.downsized_large.url);
+        });
+        return dataArr;
+    })
+    .then(data => {
+        fs.writeFileSync(dbDir, JSON.stringify(data, null, 2));
+    })
+    .catch(err => console.log('Error', err.message));
+    
 
-if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-            callback(xmlHttp.responseText);
 
 // Serve the static files from the React app
 app.use(express.static(path.join(__dirname, 'client/build')));
 
 // An api endpoint that returns a short list of items
-app.get('/api/getList', (req,res) => {
-    // var list = ["item1", "item2", "item3"];
-    res.send('hello world');
-    console.log('Sent list of items');
+app.get('/api/get-cards', (req,res) => {
+    let urls = JSON.parse(fs.readFileSync(dbDir, err => console.log(err)));
+    urls = urls.concat(urls);
+    const sendData = shuffleArray(urls);
+    res.send(sendData);
+    console.log('Data Sent');
 });
 
 // Handles any requests that don't match the ones above
-app.get('*', (req,res) =>{
+app.post('/api/leader-board', (req,res) =>{
     res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
 
